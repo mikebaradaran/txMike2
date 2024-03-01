f6558eea3acc11df6ce433fae5df560817815123
Glitch (txmike2) <none>
2024-03-01 10:05:26 +0000

    ⏱⭐️ Checkpoint ./comments.txt:34363899/9296 ./comments.js:34363899/47

--- comments.js
+++ comments.js
@@ -1,6 +1,6 @@
 function processComment(name, comment1, comment2) {
-  const duration = "2";
-  const courseTitle = "Microservices";
+  const duration = "3";
+  const courseTitle = "Web sites using React";
 
   let firstname = name.split(" ")[0];
   let com1 = "";
@@ -16,7 +16,7 @@ ${firstname} did well in this course and I am happy with ${firstname}'s progress
 ${com1}<br />Punctuality & Engagement:<br />
 ${firstname} was always punctual during the ${duration} days of the course and was engaged during the lectures.<br /><br />
 Recommendations for further learning:<br />
-Practice implementing ${courseTitle} and design at work.<br /> ${com2}<br />--------------------------------<br />`;
+Practice implementing ${courseTitle} at work.<br /> ${com2}<br />--------------------------------<br />`;
 }
 
 function deleteComments(fs){

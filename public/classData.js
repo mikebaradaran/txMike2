2213e662c32e925949b3091d465386557d82b568
Glitch (txmike2) <none>
2024-04-14 20:20:44 +0000

    ☔️🎳 Checkpoint ./public/classData.js:34363899/3841

--- public/classData.js
+++ public/classData.js
@@ -6,51 +6,73 @@ const audioFile = 'https://cdn.glitch.global/7ea2c2b4-d4b6-41d3-afca-c4c259b797b
 const afaPath = "https://qa-learning.webex.com/webappng/sites/qa-learning/dashboard?siteurl=qa-learning";
 // const afaPath = "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZjY4M2NiZTktYzJjMS00MTVkLTg1ZTktOGNkOWQzOTA3MWFl%40thread.v2/0?context=%7b%22Tid%22%3a%229c220797-c337-49f8-b086-a9a24979f728%22%2c%22Oid%22%3a%2246d9b67a-17db-40c3-ac50-6ec37a29497b%22%7d";
 var courseData = `
-Agile Software Development
-ADXD2SD3M1 
-4823475-12
-qa.webex027@qa.com
-https://onedrive.live.com/?authkey=%21AFYQekW2mymFWWA&id=82887C4F49A97ED0%21140985&cid=82887C4F49A97ED0
-GBGI9Y9WA0JU
+Introduction to Software Development - Part 2
+ADXD2SD4M1B 
+4803426-19
+qa.webex030@qa.com
+https://onedrive.live.com/?authkey=%21APtj4tSLpU8MGKI&id=82887C4F49A97ED0%21143274&cid=82887C4F49A97ED0
+N/A
 `;
 
 var trainer = "Mike Baradaran - mike.baradaran@qa.com";
 
 var passwords = `
-virtualroom14@qa.com
-QAVirtualAccess1!
-password933
+N/A
+N/A
+N/A
 `;
 
 var pcs = `
-https://www.gotomypc.com/sc/3E5F2A519D1D0B365CEF218E41DEA17F
-https://www.gotomypc.com/sc/A1EFA3F00252A45FEC2A118E41DEF244
-https://www.gotomypc.com/sc/8C540A9DA0A4C19F393E418E41DF2AF2
-https://www.gotomypc.com/sc/C818AAAB4BBF5D15E97D718E41E36CF1
-https://www.gotomypc.com/sc/E695FCBD39D557598C5FD18E41DF6879
-https://www.gotomypc.com/sc/AD769BF9B3DCA78491F2318E41DFA5C7
-https://www.gotomypc.com/sc/AB0F9D7E9F3E3252CD52E18E41DFE0B9
-https://www.gotomypc.com/sc/83747DC79DB884751ABAC18E41E0151E
-https://www.gotomypc.com/sc/7C438FC6673585FF420FD18E41E05F56
-https://www.gotomypc.com/sc/E20DEB99E0906F5CFD1D218E41E09D43
-https://www.gotomypc.com/sc/A45CF373DACCB3B5CF76F18E41E0CD3A
-https://www.gotomypc.com/sc/3E0E64DFC77C2E827C23918E41E10444
-https://www.gotomypc.com/sc/579B639338221B6E6C18818E41E14A33
-https://www.gotomypc.com/sc/6807663E837BCD75A2C1318E41E18481
-https://www.gotomypc.com/sc/BD397DC7623B5834D11AC18E41E1C1B2
-https://www.gotomypc.com/sc/FBC23C4679E7917FC493A18E41E1F8FB
-https://www.gotomypc.com/sc/1905BCEAB9B0E5E1ABD6418E41E22CB8
-https://www.gotomypc.com/sc/54DC825255F4E779297B518E41E26892
-https://www.gotomypc.com/sc/7B50074E0D3399AE903B118E41E2A309
-https://www.gotomypc.com/sc/2F1046F9D761FE6E25DA018E41E2D9AE
-https://www.gotomypc.com/sc/BDFF72676D8F83204F18318E41E30AD3
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
+https://qa.learnondemand.net
 `;
   
 var students = `
-Mike, Trainer
-Stone, Elijah	
-Brimble, Samuel	
-Barry, Jake	
-McCabe, William	
+Boughton-Shields, Ryan	
+Roberts, Benjamin	
+Claremont, Louis	
+Lawrence, Yannis
+Jaunzemis, Aiden	
+Hernandez, Nelson	
+Hassani, Sumayah	
+Tayyab, Zaki	
+Sinclair, Michael	
+McEvoy, Jane	
+Rafiee, Kamyar	
+Suaznabar, Mark	
+Matakitoga, Luke	
+Teece, Luke	
+Sharif, Yahya	
+Breslin, Colin	
+Olubi, Emmanuel	
+Jones, Yusef	
+Rawlings, Samantha	
+Mikalauskaite, Karolina	
+Patrick, Oliver	
+Fulier, Viktoria	
 `;
 

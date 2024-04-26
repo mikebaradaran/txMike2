171f474c77ea9ff6b544ed0bb60fd8e0d5d32c10
Glitch (txmike2) <none>
2024-04-26 16:30:31 +0000

    ⭐️🍢 Checkpoint ./public/classData.js:34363899/3688

--- public/classData.js
+++ public/classData.js
@@ -6,73 +6,60 @@ const audioFile = 'https://cdn.glitch.global/7ea2c2b4-d4b6-41d3-afca-c4c259b797b
 const afaPath = "https://qa-learning.webex.com/webappng/sites/qa-learning/dashboard?siteurl=qa-learning";
 // const afaPath = "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZjY4M2NiZTktYzJjMS00MTVkLTg1ZTktOGNkOWQzOTA3MWFl%40thread.v2/0?context=%7b%22Tid%22%3a%229c220797-c337-49f8-b086-a9a24979f728%22%2c%22Oid%22%3a%2246d9b67a-17db-40c3-ac50-6ec37a29497b%22%7d";
 var courseData = `
-Introduction to Software Development - Part 2
-ADXD2SD4M1B 
-4803426-19
-qa.webex030@qa.com
-https://onedrive.live.com/?authkey=%21APtj4tSLpU8MGKI&id=82887C4F49A97ED0%21143274&cid=82887C4F49A97ED0
+Design and DevOps
+ADXD2SD4M4 
+4796410-1x
+qa.webex026@qa.com
+https://onedrive.live.com/?authkey=%21AHGCZyEieufci5E&id=82887C4F49A97ED0%21140932&cid=82887C4F49A97ED0
 N/A
 `;
 
 var trainer = "Mike Baradaran - mike.baradaran@qa.com";
 
 var passwords = `
-7C5F222866924D32
-7C5F222866924D32
-7C5F222866924D32
+virtualroom25@qa.com
+QARemoteLearning#1
+password949
 `;
 
 var pcs = `
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
-https://qa.learnondemand.net
+https://www.gotomypc.com/sc/0F4F2D6C79DC36D5E578418F19DE0ED4
+https://www.gotomypc.com/sc/C8B80A97FE7D745C8FA4618F19DE51AB
+https://www.gotomypc.com/sc/2ED2D53FBD0CE4057E0C118F19DFFB8D
+https://www.gotomypc.com/sc/A93F5E6D4DEA0E9A778C218F19E01DF9
+https://www.gotomypc.com/sc/9FB55A086CA01A6AB733018F19DE898B
+https://www.gotomypc.com/sc/D0DF66D469BC67A29853618F19E069E2
+https://www.gotomypc.com/sc/0DF386BF74BF6DC9E8B4318F19DEAD3A
+https://www.gotomypc.com/sc/02A80D24E49EE8585E94318F19E0B3A9
+https://www.gotomypc.com/sc/0165F23E0DF790046C88D18F19E0D6B6
+https://www.gotomypc.com/sc/7551D6335E7B1D7B1569A18F19E0F667
+https://www.gotomypc.com/sc/B7CAB9C91FEAC0755F71618F19E11B39
+https://www.gotomypc.com/sc/245A246ED6F3BA6B6412218F19E14170
+https://www.gotomypc.com/sc/CE821A8179CBD5784656218F19E166A0
+https://www.gotomypc.com/sc/557C106A4FC47CC27A90F18F19DECF0F
+https://www.gotomypc.com/sc/C545C4A229A4EC38F417D18F19DEF5E0
+https://www.gotomypc.com/sc/AB1EE322BEE45C15E4F1B18F19E1DABD
+https://www.gotomypc.com/sc/663008DFE0E210468A2E018F19E200A2
+https://www.gotomypc.com/sc/615FC2BE348E5495DB0F718F19E22303
+https://www.gotomypc.com/sc/BEA406A8FED5317199B0218F19E242A0
 `;
   
 var students = `
-Boughton-Shields, Ryan	
-Roberts, Benjamin	
-Claremont, Louis	
-Lawrence, Yannis
-Jaunzemis, Aiden	
-Hernandez, Nelson	
-Hassani, Sumayah	
-Tayyab, Zaki	
-Sinclair, Michael	
-McEvoy, Jane	
-Rafiee, Kamyar	
-Suaznabar, Mark	
-Matakitoga, Luke M	
-Teece, Luke T	
-Sharif, Yahya	
-Breslin, Colin	
-Olubi, Emmanuel	
-Jones, Yusef	
-Rawlings, Samantha	
-Mikalauskaite, Karolina	
-Patrick, Oliver	
-Fulier, Viktoria	
-`;
+Thomas, Eliza	
+Harker, Alexandra	
+McGurk, Adam	
+Hayward, Paul	
+Dey, Nandan	
+Bell, Steven	
+Whitcombe-Ritchie, Jacob	
+Metcalfe, Samuel	
+Georgiev, Ivaylo	
+Weekes, Patrick	
+Fratiman, Alexandra	
+Robinson, Nathan	
+Jarrett, Andrew	
+Cale, Joanna	
+Brooks, Jasmine	
+Martin, Jonathan	
+Ribbens, Thomas`;
 

2b6ba09634513cba1a24694abedc1beae2520ca3
Glitch (txmike2) <none>
2024-06-16 17:16:36 +0000

    👼⛴ Checkpoint ./public/timer.js:34363899/2903 ./views/index.ejs:34363899/152

--- public/timer.js
+++ public/timer.js
@@ -1,42 +1,71 @@
-const audioFile = 'https://cdn.glitch.global/7ea2c2b4-d4b6-41d3-afca-c4c259b797be/Alarm01.wav?v=1685964726574';
 
-function setTime(mins){
-  document.getElementById('timer').value = mins;
+qaTimer("qa-timer");
+
+export function setTimerValue(seconds){
+  getTimer().value = seconds;
   startTimer();
 }
 
-var myTimer = null;
+function qaTimer(tagName) {
+    document.getElementsByTagName(tagName)[0].innerHTML = 
+`<span id="startBtn">▶️</span>
+<input type='range' min='0' max='3600' value='0' id='myTimer'/>
+<output id='output'>00:00</output>`;
+
+    document.getElementById('startBtn').addEventListener('click', startTimer);
+    getTimer().addEventListener('input', showTime);
+}
+
 function stopTimer() {
-  if (myTimer !== null) clearInterval(myTimer);
+    let stopWatch = getTimer().stopWatch;
+    if (stopWatch !== null)
+        clearInterval(stopWatch);
 }
+
 function startTimer() {
-  let mins = parseInt(document.getElementById('timer').value);
-  if (mins < 0) return;
-
-  stopTimer();
-  showTime("");
-
-  let seconds = mins * 60;
-
-  myTimer = setInterval(function () {
-    var minutes = (seconds / 60) | 0;
-
-    if (seconds < 0) {
-      stopTimer();
-      showTime(mins + " minutes passed. Ended at " + getTime());
-      new Audio(audioFile).play();
-      return;
-    }
-    document.getElementById('divTimer').innerHTML = minutes + ":" + (seconds - minutes * 60);
-    seconds--;
-  }, 1000);
+    stopTimer();
+    let seconds = parseInt(getTimer().value);
+    let myTimer = getTimer();
+    myTimer.stopWatch = setInterval(function () {
+        if (seconds <= 0) {
+            timerFinishedAction();
+            return;
+        }
+        myTimer.value = --seconds;
+        showTime();
+    }, 1000);
 }
 
-function showTime(msg){
-  document.getElementById("message").innerHTML = msg;
+//---------------------------------------------------------------------
+function timerFinishedAction() {
+    const audio = 'https://cdn.glitch.global/7ea2c2b4-d4b6-41d3-afca-c4c259b797be/Alarm01.wav';
+    stopTimer();
+    display( "Ended at " + getTime() );
+    new Audio(audio).play();
+}
+
+function showTime() {
+    display( getMinSec() );
+}
+
+function getMinSec() {
+    let seconds = parseInt(getTimer().value);
+    return Math.floor(seconds / 60) + ":" + seconds % 60;
 }
 
 function getTime() {
-  var today = new Date();
-  return today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var today = new Date();
+    return today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+}
+
+function getTimer() {
+    return document.getElementById('myTimer');
+}
+
+function getOutput() {
+    return document.getElementById('output');
+}
+
+function display(msg) {
+    getOutput().innerHTML = msg;
 }
